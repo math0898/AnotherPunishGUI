5935390a4a426490551f08a69ec46dd337bcae20
math0898 <25396616+math0898@users.noreply.github.com>
2024-04-03 16:38:50 -0500

    LiteBans not LightAntiCheat

--- src/main/java/io/github/math0898/anotherpunishgui/punisher/LightAntiCheatPunisher.java
+++ src/main/java/io/github/math0898/anotherpunishgui/punisher/LightAntiCheatPunisher.java
@@ -1,11 +1,11 @@
 package io.github.math0898.anotherpunishgui.punisher;
 
 /**
- * The LightAntiCheatPunisher is used when LightAntiCheat is found on the server and configured to be used instead of
+ * The LiteBansPunisher is used when LiteBans is found on the server and configured to be used instead of
  * the built-in punisher.
  *
  * @author Sugaku
  */
-public class LightAntiCheatPunisher implements Punisher {
+public class LiteBansPunisher implements Punisher {
 
 }
