8a1f32afd03555e83675ec05d79e70dcf876f2dc
math0898 <25396616+math0898@users.noreply.github.com>
2024-04-04 15:14:20 -0500

    Started Reports GUI.

---
+++ src/main/java/io/github/math0898/anotherpunishgui/gui/ReportsGUI.java
@@ -0,0 +1,52 @@
+package io.github.math0898.anotherpunishgui.gui;
+
+import io.github.math0898.anotherpunishgui.database.DatabaseProvider;
+import io.github.math0898.anotherpunishgui.structures.Report;
+import io.github.math0898.utils.gui.PageableGUI;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The ReportsGUI shows a staff member a full list of reports that have been given out.
+ *
+ * @author Sugaku
+ */
+public class ReportsGUI extends PageableGUI {
+
+    /**
+     * Creates a new PageableGUI with the given name.
+     */
+    public ReportsGUI () {
+        super(ChatColor.DARK_GRAY + "Reports");
+
+        setItems();
+    }
+
+    /**
+     * Opens this GUI to the given player.
+     *
+     * @param player The player to open the GUI to.
+     */
+    @Override
+    public void openInventory (Player player) {
+        List<Report> reports = DatabaseProvider.getInstance().getDatabase().getReports();
+        List<ItemStack> itemDisplay = new ArrayList<>();
+        reports.forEach((r) -> itemDisplay.add(r.getItemStack()));
+        super.openInventory(player);
+    }
+
+    /**
+     * Called whenever this GUI is clicked.
+     *
+     * @param event The inventory click event.
+     */
+    @Override
+    public void onClick(InventoryClickEvent event) {
+        super.onClick(event);
+    }
+}
