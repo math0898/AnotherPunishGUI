6820a342b48d1cc8b6469997a346d145776ca896
math0898 <25396616+math0898@users.noreply.github.com>
2024-04-06 13:11:10 -0500

    Bug Fixes.

--- src/main/java/io/github/math0898/anotherpunishgui/punisher/StandalonePunisher.java
+++ src/main/java/io/github/math0898/anotherpunishgui/punisher/StandalonePunisher.java
@@ -6,6 +6,7 @@ import io.github.math0898.anotherpunishgui.database.DatabaseProvider;
 import io.github.math0898.anotherpunishgui.structures.Log;
 import io.github.math0898.anotherpunishgui.structures.Punishment;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -27,9 +28,9 @@ public class StandalonePunisher implements Punisher {
     public void punish (Punishment punishment, CommandSender staff, Player player) {
         int durIndex = punishment.getDurationIndex(player);
         // /ban is a vanilla Minecraft command but does not support durations. In this case we're fishing for another provider such as Essentials without checking.
-        if (punishment.type().equalsIgnoreCase("ban")) Bukkit.getServer().dispatchCommand(staff, "ban " + player.getName() + " " + punishment.durations().get(durIndex) + "h You have been banned for " + punishment.displayName() + ". (" + punishment.displayDuration(durIndex) + ")");
+        if (punishment.type().equalsIgnoreCase("ban")) Bukkit.getServer().dispatchCommand(staff, "ban " + player.getName() + " " + punishment.durations().get(durIndex) + "h You have been banned for " + punishment.displayName() + ChatColor.RESET + ". (" + punishment.displayDuration(durIndex) + ")");
         // /mute is not a vanilla Minecraft command. In this case we're fishing for another provider such as Essentials without checking.
-        else if (punishment.type().equalsIgnoreCase("mute")) Bukkit.getServer().dispatchCommand(staff, "mute " + player.getName() + " " + punishment.durations().get(durIndex) + "h You have been muted for " + punishment.displayName() + ". (" + punishment.displayDuration(durIndex) + ")");
+        else if (punishment.type().equalsIgnoreCase("mute")) Bukkit.getServer().dispatchCommand(staff, "mute " + player.getName() + " " + punishment.durations().get(durIndex) + "h You have been muted for " + punishment.displayName() + ChatColor.RESET + ". (" + punishment.displayDuration(durIndex) + ")");
         // In either of these cases AnotherPunishGUI could enact the punishment fairly easily but this project has already expanded too much.
         Log log = new Log(staff.getName(), player.getUniqueId(), punishment.internalName(), punishment.type(), System.currentTimeMillis(), punishment.durations().get(durIndex) * 60 * 60 * 1000);
         Database database = DatabaseProvider.getInstance().getDatabase();
